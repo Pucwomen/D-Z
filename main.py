35f525defa826044758c7ad5654fd1e50a983dd4
Pucwomen <qwertzxc345456@gmail.com>
2024-10-23 02:44:47 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+name="number of homework completed"
+print(name)
+name=12
+print("12")
+name="number of hours worked"
+print(name)
+name=1.5
+print(name)
+name="course name"
+print(name)
+name=("Pyhton")
+print(name)
+name="time for one task"
+print(name)
+print(1.5/12)
+
+
+
